2bdc97d2cb8a467acc55730bd75f963f2ba46d7e
SuperDroidBond <superdroidbond@yahoo.com>
2018-09-06 12:19:32 +0530

    Initiate DerpZone for Q
    *Migrate all v7 & v4 preferences to androidx

---
+++ src/com/derp/caf/fragments/NotificationSettings.java
@@ -0,0 +1,32 @@
+package com.derp.caf.fragments;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class NotificationSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        addPreferencesFromResource(R.xml.derp_settings_notifications);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DERPZONE;
+    }
+}
