08da35ad653a4e38cc28088ceaacca77a8a8f5b6
ZyxxeZ <bankersenator@gmail.com>
2019-08-08 10:26:42 +0000

    DerpZone: recents cleanup

--- src/com/derpcaf/derpzone/fragments/RecentsSettings.java
+++ src/com/derpcaf/derpzone/fragments/RecentsSettings.java
@@ -8,12 +8,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.support.v7.preference.Preference;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -28,21 +27,20 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String IMMERSIVE_RECENTS = "immersive_recents";
-    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
-    private static final String RECENTS_TYPE = "recents_layout_style";
-    private static final int RECENTS_COMPONENT_OREO = 1;
+    private static final String RECENTS_LAYOUT_STYLE_PREF = "recents_layout_style";
 
-    private ListPreference mRecentsComponentType;
+    private ListPreference mRecentsLayoutStylePref;
     private ListPreference mImmersiveRecents;
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
-    private ListPreference mRecentsType;
-    private PreferenceCategory oreoRecentsCat;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
+	mFooterPreferenceMixin.createFooterPreference()
+		.setTitle(R.string.dumb_dev);
+	
         addPreferencesFromResource(R.xml.derpzone_recents);
 
         ContentResolver resolver = getActivity().getContentResolver();
@@ -55,30 +53,19 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
 
-        // recents component type
-        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
-        int type = Settings.System.getInt(resolver,
-                Settings.System.RECENTS_COMPONENT, 0);
-        mRecentsComponentType.setValue(String.valueOf(type));
-        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
-        mRecentsComponentType.setOnPreferenceChangeListener(this);
-        oreoRecentsCat = (PreferenceCategory)findPreference("recents_ui_oreo_recents_category");
-        oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
-
-        // oreo recents type
-        mRecentsType = (ListPreference) findPreference(RECENTS_TYPE);
-        int style = Settings.System.getIntForUser(resolver,
-                Settings.System.RECENTS_LAYOUT_STYLE, 0, UserHandle.USER_CURRENT);
-        mRecentsType.setValue(String.valueOf(style));
-        mRecentsType.setSummary(mRecentsType.getEntry());
-        mRecentsType.setOnPreferenceChangeListener(this);
-
         mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
         mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
                 resolver, Settings.System.IMMERSIVE_RECENTS, 0)));
         mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
         mImmersiveRecents.setOnPreferenceChangeListener(this);
 
+        // recents layout style
+        mRecentsLayoutStylePref = (ListPreference) findPreference(RECENTS_LAYOUT_STYLE_PREF);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_LAYOUT_STYLE, 0);
+        mRecentsLayoutStylePref.setValue(String.valueOf(type));
+        mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntry());
+        mRecentsLayoutStylePref.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -97,22 +84,13 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
             mImmersiveRecents.setValue(String.valueOf(objValue));
             mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
             return true;
-        } else if (preference == mRecentsType) {
-            int style = Integer.valueOf((String) objValue);
-            int index = mRecentsType.findIndexOfValue((String) objValue);
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.RECENTS_LAYOUT_STYLE, style, UserHandle.USER_CURRENT);
-            mRecentsType.setSummary(mRecentsType.getEntries()[index]);
-            DerpcafUtils.showSystemUiRestartDialog(getContext());
-        return true;
-        } else if (preference == mRecentsComponentType) {
+        } else if (preference == mRecentsLayoutStylePref) {
             int type = Integer.valueOf((String) objValue);
-            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            int index = mRecentsLayoutStylePref.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.RECENTS_COMPONENT, type);
-            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
-            oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
-            if (type == RECENTS_COMPONENT_OREO) { // Disable swipe up gesture, if oreo type selected
+                    Settings.System.RECENTS_LAYOUT_STYLE, type);
+            mRecentsLayoutStylePref.setSummary(mRecentsLayoutStylePref.getEntries()[index]);
+            if (type !=0) { // Disable swipe up gesture, if oreo type selected
                Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
